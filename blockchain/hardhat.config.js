b48bb16b81d988a87c646fa6a0f84315a75385fc
piachini <pierluigi.iachini@marconicloud.it>
2025-04-17 23:35:00 +0200

    ci: add solidity coverage configuration

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
